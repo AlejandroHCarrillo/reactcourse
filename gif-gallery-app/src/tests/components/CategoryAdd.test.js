810a366ef5ee0935190359b3509f4d65f98ea63d
Alejandro <ahernandezdotnet@gmail.com>
2021-01-02 19:27:20 -0600

    Prueba de verificacion de que una funcion ha sido llamada

--- gif-gallery-app/src/tests/components/CategoryAdd.test.js
+++ gif-gallery-app/src/tests/components/CategoryAdd.test.js
@@ -1,10 +1,17 @@
 import React from 'react';
+import '@testing-library/jest-dom'; // libreria que sirve para tener la ayuda del expect
 import { shallow } from "enzyme";
 import { CategoryAdd } from '../../components/CategoryAdd'
 
 describe('Probando el componente CategoryAdd', () => {
-    const setCategories = () => {}; // funcion dummy para cumplir el parametro requerido
-    const wrapper = shallow(<CategoryAdd setCategories = {setCategories}/>);
+    // const setCategories = () => {}; // funcion dummy para cumplir el parametro requerido
+    const setCategories = jest.fn(); // funcion dummy usando jest para cumplir el parametro requerido
+    let wrapper = shallow(<CategoryAdd setCategories = {setCategories}/>);
+
+    beforeEach(() => {
+        jest.clearAllMocks();
+        wrapper = shallow(<CategoryAdd setCategories = {setCategories}/>);
+    });
 
     test('el componente CategoryAdd debe de mostrarse ', () => {
         expect( wrapper ).toMatchSnapshot();
@@ -16,6 +23,15 @@ describe('Probando el componente CategoryAdd', () => {
         txtInput.simulate('change', { target: { value: expValue }});
 
         expect( wrapper.find('h2').text().trim() ).toBe( expValue );
+    })
+    
+    test('no debe de postear on submit ', () => {
+        // el segundo parametro de simulate son los parametros enviados
+        // podemos mandar la funcion de esta manera { preventDefault: ()=>{} } 
+        // o de esta manera { preventDefault(){} }
+        wrapper.find('form').simulate('submit', { preventDefault(){} });
+
+        expect(setCategories).not.toHaveBeenCalled()
     })
     
 
