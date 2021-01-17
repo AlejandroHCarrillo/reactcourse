ebfb7eb1e2bd21f6b2e2cbd80d678ddf703aa68f
Alejandro <ahernandezdotnet@gmail.com>
2021-01-16 20:08:58 -0600

    Crecion de rutas y paginas pricipales
    * Router principal (AppRouter)
    * Router Secundario (DashboardRouter)

---
+++ heroes-app/src/components/marvel/MarvelScreen.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const MarvelScreen = () => {
+    return (
+        <>
+            <h1>Marvel o default</h1>
+        </>
+    )
+}
---
+++ heroes-app/src/components/routers/DashboardRoutes.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { Redirect, Route, Switch } from 'react-router-dom';
+import { DcScreen } from '../dc/DcScreen';
+import { HeroScreen } from '../heroes/HeroScreen';
+import { MarvelScreen } from '../marvel/MarvelScreen';
+import { Navbar } from '../ui/Navbar';
+
+export const DashboardRoutes = () => {
+    return (
+        <>
+            <Navbar />
+
+            <div>
+                <Switch>
+                    <Route exact path="/marvel" component={ MarvelScreen } />
+                    <Route exact path="/heroe/:heroeId" component={ HeroScreen } />
+                    <Route exact path="/dc" component={ DcScreen } />
+
+                    <Redirect to="/marvel"/>
+                </Switch>
+            </div>
+        </>
+    )
+}
